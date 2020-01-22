bef081f1aeb6ae00285674ab0b1b6871ad28bfb8
jenkins-Ventron <jenkins@ventron.com.br>
2020-01-22 19:11:21 -0300

    Aula 2 22/01 30:00

---
+++ aulas/backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const routes = express.Router();
+
+
+routes.get('/', (req, res)  => {
+    return res.json({ message: `Hello ${req.query.name}`});
+});
+
+routes.post('/devs', (req, res) => {
+   console.log(req.body);
+   
+    return res.json({ ok: true });
+});
+
+module.exports = routes;
