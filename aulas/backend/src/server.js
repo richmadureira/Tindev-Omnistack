ecdc628292fc57ab8c52d75489fd5a02f0bcb57e
jenkins-Ventron <jenkins@ventron.com.br>
2020-01-24 18:57:08 -0300

    aula 2 00:47:22

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes'); 
 
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistack:123@cluster0-vjdxx.azure.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+ 
 server.use(express.json());
 server.use(routes);
 
