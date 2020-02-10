366b34f42526c252b9557f16c61bbd903cd29242
jenkins-Ventron <jenkins@ventron.com.br>
2020-02-10 18:26:59 -0300

    00:47 aula 3

--- aulas/frontend/src/pages/Login.js
+++ aulas/frontend/src/pages/Login.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 import './Login.css';
 
+import api from '../services/api'
+
 import logo from '../assets/logo.svg';
 
-export default function Login(){
+export default function Login({ history }){
     const [username, setUsername] = useState('');
 
 
-function handleSubmit(e) {
-    e.preventDefault();
+    async function handleSubmit(e) {
+        e.preventDefault();
 
-    console.log(username); 
-}
+        const response = await api.post('http://localhost:3333/devs', {
+            username,
+        })
+
+        console.log(response); 
+
+        history.push('/main');
+    }
 
 
     return (
       <div className="login-container">
           <form onSubmit={handleSubmit}>
               <img src={logo} alt="Tindev" />
-              <input placeholder="Digite seu usuário no Github"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
+              <input 
+                placeholder="Digite seu usuário no Github"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
               />
               <button type="submit">Enviar</button>
           </form>
